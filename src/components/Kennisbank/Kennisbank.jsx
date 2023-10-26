1353199274727cbe7f0c56a8e8daa443744c3f1d
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-10-26 20:41:00 +0200

    Data fetching gelukt

--- src/components/Kennisbank/Kennisbank.jsx
+++ src/components/Kennisbank/Kennisbank.jsx
@@ -1,25 +1,56 @@
 import '../Kennisbank/Kennisbank.css';
 
-export async function fetchData() {
-  try {
-    const response = await fetch('http://localhost:8055/items/kennisbank');
-    const data = await response.json();
-    console.log(data);
-    return { data };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw { error };
-  }
-}
+const data = await fetch('http://localhost:8055/items/kennisbank').then((response) =>
+  response.json()
+);
 
-const Kennisbank = ({ data }) => {
-  return (
-    <div>
-       <h3>{data.id}</h3>
-      <h3>{data.title}</h3>
-     <h3>{data.content}</h3>
+const Kennisbank = () => {
+ // Access the data array from the object
+ const dataArray = data.data;
+
+ // Assuming there's only one item in the array, access its title
+ const title = dataArray.length > 0 ? dataArray[0].title : '';
+ const content = dataArray.length > 0 ? dataArray[0].content : '';
+
+
+ return (
+
+<article class="main-post">
+       
+{/* {#if dataArray[0]} */}
+  
+{/* <img src={lastPost1.image.url} alt="" /> */}
+
+  <div className="content-field">
+    <span className="category">klimaatadaptatie</span>
+    <span className="author">Door Timo op 2 juni</span>
+    <h3>{title}</h3>
+   <div className="hygraph-html" dangerouslySetInnerHTML={{ __html: content }} />
     </div>
-  );
+
+    <div class="read-more-container">
+      <a class="read-more-link" href="">Verder lezen</a>
+    </div>
+{/* // {/if} */}
+</article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ );
 };
 
-export { Kennisbank };
+export default Kennisbank;
