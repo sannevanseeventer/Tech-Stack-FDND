1f116d951d343398ae43777bb65df917437dbb45
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-10-27 12:50:22 +0200

    dataArray blogposts
    Blogposts komen binnen vanuit Directus

--- src/components/Kennisbank/Kennisbank.jsx
+++ src/components/Kennisbank/Kennisbank.jsx
@@ -5,54 +5,33 @@ const data = await fetch('http://localhost:8055/items/kennisbank').then((respons
 );
 
 const Kennisbank = () => {
- // Access the data array from the object
- const dataArray = data.data;
+  // Access the data array from the object
+  const dataArray = data.data;
 
- // Assuming there's only one item in the array, access its title
- const title = dataArray.length > 0 ? dataArray[0].title : '';
- const content = dataArray.length > 0 ? dataArray[0].content : '';
- const category = dataArray.length > 0 ? dataArray[0].category : '';
- const author = dataArray.length > 0 ? dataArray[0].author : '';
+  return (
+    <div className="Kennisbank">
+      <h2>Kennisbank</h2>
 
+      {dataArray.map((item) => (
+        <article key={item.id} className="main-post">
+          <img className="image-blog" src={`http://localhost:8055/assets/${item.blog_image}`} alt={item.title} />
 
- return (
+          <div className="content-field">
+            <span className="category">{item.category}</span>
+            <span className="author">Door {item.author}</span>
+            <h3>{item.title}</h3>
+            <div className="hygraph-html" dangerouslySetInnerHTML={{ __html: item.content }} />
+          </div>
 
-<article className="main-post">
-       
-{/* {#if dataArray[0]} */}
-  
-{/* <img src={lastPost1.image.url} alt="" /> */}
-
-  <div className="content-field">
-    <span className="category">{category}</span>
-    <span className="author">Door {author}</span>
-    <h3>{title}</h3>
-   <div className="hygraph-html" dangerouslySetInnerHTML={{ __html: content }} />
+          <div className="read-more-container">
+            <a className="read-more-link" href="">
+              Verder lezen
+            </a>
+          </div>
+        </article>
+      ))}
     </div>
-
-    <div className="read-more-container">
-      <a className="read-more-link" href="">Verder lezen</a>
-    </div>
-{/* // {/if} */}
-</article>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- );
+  );
 };
 
 export default Kennisbank;
